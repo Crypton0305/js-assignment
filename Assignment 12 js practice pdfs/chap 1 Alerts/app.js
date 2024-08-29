0d0d8088810444a6f7af594d3bb1555cad5c234d
Ayan Ahmed <136894145+Crypton0305@users.noreply.github.com>
2024-08-29 13:53:07 +0500

    pending

---
+++ Assignment 12 js practice pdfs/chap 1 Alerts/app.js
@@ -0,0 +1,7 @@
+alert("hi");
+ 
+
+alert("welcome to my page")
+ 
+
+alert("its a demo")
---
+++ Assignment 12 js practice pdfs/chap 4 legal or illegal/index.js
@@ -0,0 +1,18 @@
+// StartQuestion 1 
+
+var name = "Ayan ahmed", age = 17, city = "Karachi";
+
+// Start Question 2 
+
+var legal_Var = "value1";
+var legalVar_ = "value2";
+var $legalVar = "value3";
+var legal_var4 = "value4";
+var legalVarFive = "value5";
+
+// Start Question 3 
+document.write("<h1>Rules for Naming JS Variables</h1>");
+document.write("<p>Variable names can only contain <strong>letters</strong>, <strong>numbers</strong>, <strong>$</strong>, and <strong>_</strong>. For example: <strong>$my_1stVariable</strong></p>");
+document.write("<p>Variables must begin with a <strong>letter</strong>, <strong>$</strong>, or <strong>_</strong>.  For example: <strong>$name</strong>, <strong>_name</strong> or <strong>name</strong></p>");
+document.write("<p>Variable names are case <strong>sensitive</strong>.</p>");
+document.write("<p>Variable names should not be JS <strong>keywords</strong>.</p>");
